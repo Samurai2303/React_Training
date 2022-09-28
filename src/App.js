33d1f78587e17fedf30c6be6f4e216b090429850
Samurai2303 <meleschenko.andriy@gmail.com>
2022-09-28 19:03:27 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
       <div>
+          <Routes>
+              <Route path={'/'} element={} />
+          </Routes>
 
       </div>
   );
